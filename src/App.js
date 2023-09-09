bedd37a97bafb9310d2ac3356332e6b8cfae7186
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-09 14:34:20 +0500

    feat: made nav bar and hero section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import * as React from 'react'
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Image } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-      <header className="h-18 flex items-center justify-center bg-red-300">
-        <p className="text-3xl font-bold">VEX.</p>
-      </header>
-      
-      <Button colorScheme='teal' size='lg'>
-        Button
-      </Button>
+        
+        <nav class="fixed top-0 h-20 w-full flex justify-between items-center text-white">
+          <div>
+            <img src='../public/images/whiteLogo.png'/>
+          </div>  
+          <div>
+            <ul className='flex text-md font-semibold px-10 space-x-5'> 
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>  
+        </nav>
+        <main className='w-full overflow-hidden'>
+          <div className='bg-gradient-to-r from-black to-[#160b35] w-[200%] ml-[-50%] h-[50vw] rounded-b-[100%] text-white pl-[50%]'>
+            <div className='pt-20 px-10 text-white text-xl'>
+              VEX
+            </div>
+          </div>
+        </main>
       </div>
     </ChakraProvider>
   );
