2963bfab887f0a897e518fd93c7715cc699df87f
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-09 12:35:53 +0500

    chore: configure tailwind and chakra UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 function App() {
   return (
-    <div className="App">
-      Vex
-    </div>
+    <ChakraProvider>
+      <div className="App">
+      <header className="h-18 flex items-center justify-center bg-red-300">
+        <p className="text-3xl font-bold">VEX.</p>
+      </header>
+      
+      <Button colorScheme='teal' size='lg'>
+        Button
+      </Button>
+      </div>
+    </ChakraProvider>
   );
 }
 
