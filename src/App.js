1cc47372f4853ddf319a8fef38d0a79e503b7f13
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-11 19:48:16 +0500

    fix: Changes in nav, hero section and pricing plan.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <nav class="absolute max-sm:h-10 max-sm:px-8 sm:h-16 mt-7 sm:px-16 lg:h-24 lg:px-24 w-full flex justify-between items-center text-white">
+        <nav class="absolute max-sm:h-10 max-sm:px-8 sm:h-16 mt-7 sm:px-16 lg:h-24 lg:px-24 2xl:px-52 w-full flex justify-between items-center text-white">
           <div>
             <img src={whiteLogo} className='max-sm:w-14 sm:w-20'/>
           </div>  
@@ -37,27 +37,32 @@ function App() {
           </div> 
         </nav>
         <main className='w-full overflow-hidden'>
-          <div className='bg-gradient-to-r from-black to-black max-sm:w-[180%] w-[120%] ml-[-10%] h-[550px] rounded-b-[100%] text-white max-sm:ml-[-40%] max-sm:pl-[40%] pl-[10%]'>
+          <div className='bg-gradient-to-r from-black to-black max-md:h-[500px] max-md:w-[180%] max-md:ml-[-40%] max-md:pl-[40%] md:w-[160%] md:ml-[-30%] md:pl-[30%] md:h-[530px] lg:w-[120%] lg:pl-[10%] lg:ml-[-10%] lg:h-[600px] rounded-b-[100%] text-white'>
 
-            <div className='absolute grid max-sm:px-8 max-sm:mt-28 max-sm:grid-cols-1 grid-cols-3 sm:px-16 sm:mt-10 lg:px-24 lg:mt-14 w-full text-white'>
-              <div className='col-span-2 max-sm:space-y-5 sm:pt-20 sm:space-y-5 lg:space-y-8'>
-                  <h1 className='max-sm:text-3xl sm:text-3xl lg:text-4xl lg:font-light font-extralight w-[90%] leading-snug'>Unlock Your Future with a Powerful Personal Portfolio</h1>
+            <div className='absolute grid max-sm:px-8 max-sm:mt-28 max-sm:grid-cols-1 grid-cols-3 sm:px-16 sm:mt-16 lg:px-24 lg:mt-20 2xl:px-52 w-full text-white'>
+              <div className='col-span-2 sm:pt-20'>
+                  <div className='sm:space-y-5 max-sm:space-y-5 lg:space-y-8'>
+                    <h1 className='max-sm:text-3xl sm:text-3xl lg:text-4xl lg:font-light font-extralight xl:w-[80%] 2xl:w-[70%] w-[90%] leading-snug'>Unlock Your Future with a Powerful Personal Portfolio</h1>
 
-                  <p className='max-sm:text-md sm:text-md lg:text-xl text-justify font-extralight w-[90%] leading-relaxed'>Create Your Stunning Portfolio with VEX. Easy Templates, Hassle-Free Hosting.<br/>Discover Beautiful Templates and Seamless Hosting to Make Your Portfolio Shine.</p>
-                  <button className='bg-white max-sm:py-3 max-sm:px-3 max-sm:text-sm sm:px-3 sm:py-1 lg:px-5 lg:py-2 flex justify-center items-center text-black sm:text-md lg:text-lg font-bold border-0 rounded-full'>Get Started <img src={rightArrow} className='max-sm:w-5 sm:w-9'/>
+                    <p className='max-sm:text-clip max-sm:text-md sm:text-md lg:text-xl font-extralight xl:w-[80%] 2xl:w-[70%] w-[90%] leading-relaxed break-words'>Create Your Stunning Portfolio with VEX. Easy Templates, Hassle-Free Hosting.Discover Beautiful Templates and Seamless Hosting to Make Your Portfolio Shine.</p>
+                  </div>
+                  <button className='bg-white max-sm:py-3 max-sm:px-3 max-sm:text-sm mt-10 sm:px-3 sm:py-3 lg:px-5 lg:py-4 flex justify-center items-center text-black sm:text-md lg:text-lg font-bold border-0 rounded-full'>
+                    <div className='flex sm:items-center'>
+                      Get Started <img src={rightArrow} className='ml-3 max-sm:w-5 sm:w-9'/>
+                    </div>
                   </button>
               </div>
-              <div className='max-sm:hidden flex w-full justify-center lg:pr-20 lg:pb-28'>
-                  <img src={guyImage} className='max-sm:h-fit max-sm:p-10 sm:w-64 sm:pt-10 lg:pt-16 xl:w-96 xl:p-10 2xl:w-80'/>
+              <div className='max-sm:hidden flex w-full items-center justify-center'>
+                  <img src={guyImage} className='sm:h-fit sm:pt-10 lg:w-[286px] 2xl:w-80'/>
               </div>
             </div>
           </div>
         </main>
 
-        <section className='mt-10 max-sm:px-8 sm:px-16 lg:px-24'>
+        <section className='mt-10 max-sm:px-8 sm:px-16 lg:px-24 2xl:px-52'>
           <div className='text-center max-sm:text-3xl text-5xl font-bold'>Pricing Plans</div> 
-          <div className='mt-10 max-md:block grid grid-cols-2 gap-10'>
-            <div className='w-full my-10 space-y-3 p-8 rounded-3xl shadow-black shadow-lg'>
+          <div className='mt-10 max-md:block lg:flex lg:space-x-9 lg:justify-center grid grid-cols-2 gap-10'>
+            <div className='lg:w-[400px] w-full my-10 space-y-3 p-8 rounded-3xl shadow-black shadow-lg'>
               <div className='flex space-x-3'>
                 <div className='w-7 h-7 p-2 border-black border-[0.5px] rounded-full'>
                   <img src={favicon}/>
@@ -92,7 +97,7 @@ function App() {
               <div className='text-xs text-justify'>* charges applies after 5 editing permissions are utilized. To avoid this, please verify the data before sending it to our team, Thank you!</div>
             </div>
             
-            <div className='w-full my-10 space-y-3 p-8 bg-[#141010] rounded-3xl shadow-black shadow-lg text-white'>
+            <div className='lg:w-[400px] w-full my-10 space-y-3 p-8 bg-[#141010] rounded-3xl shadow-black shadow-lg text-white'>
               <div className='flex space-x-3'>
                 <div className='w-7 h-7 p-2 border-black border-[0.5px] bg-white rounded-full'>
                   <img src={favicon}/>
@@ -131,7 +136,7 @@ function App() {
           
         </section>
 
-        <footer className='mt-10 h-20 max-sm:px-8 sm:px-16 lg:px-24'>
+        <footer className='mt-10 h-20 max-sm:px-8 sm:px-16 lg:px-24 2xl:px-52'>
           <div className='flex h-20 justify-between items-center'>
             <div>© Vex Co. 2023</div>
             <div className='flex space-x-3'>
