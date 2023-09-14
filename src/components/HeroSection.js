5552fe34e0fcd678d3a39b6e9159c92c1f6c2516
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-14 20:06:30 +0500

    fix: added z index so that menu can float on hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ const HeroSection = (props) => {
       <main className='w-full max-sm:-mt-20 sm:-mt-28 py-10 md:-mt-32 lg:-mt-40 overflow-hidden'>
           <div className='bg-black max-md:h-[500px] max-md:w-[180%] max-md:ml-[-40%] max-md:pl-[40%] md:w-[160%] md:ml-[-30%] md:pl-[30%] md:h-[530px] lg:w-[120%] lg:pl-[10%] lg:ml-[-10%] lg:h-[600px] rounded-b-[100%] text-white'>
 
-            <div className='absolute grid max-sm:px-8 max-sm:mt-28 max-sm:grid-cols-1 grid-cols-3 sm:px-16 sm:mt-16 lg:px-24 lg:mt-20 2xl:px-52 w-full text-white'>
+            <div className='absolute z-0 grid max-sm:px-8 max-sm:mt-28 max-sm:grid-cols-1 grid-cols-3 sm:px-16 sm:mt-16 lg:px-24 lg:mt-20 2xl:px-52 w-full text-white'>
               <div className='col-span-2 max-sm:-mt-10 sm:pt-16 md:pt-20'>
                   <div className='sm:space-y-5 max-sm:space-y-5 lg:space-y-8'>
                     <h1 className='max-sm:text-3xl sm:text-3xl lg:text-4xl lg:font-light font-extralight xl:w-[80%] 2xl:w-[70%] w-[90%] leading-snug'>Unlock Your Future with a Powerful Personal Portfolio</h1>
