f437a0a25d205966bdf0a2418010cb2fa3a6bcf8
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-17 15:21:28 +0500

    feat: add attari template view

---
+++ src/components/templatesView.js/AttariView.js
@@ -0,0 +1,12 @@
+import React from "react"
+import attari from "H:\\Github\\vex-web\\src\\images\\templates\\attari.jpg"
+
+const CosmosView = (props) => {
+  return (
+    <div className="mt-10 mb-20 flex justify-center max-sm:px-8 sm:px-16 lg:px-24 2xl:px-52">
+      <img src={attari}/>
+    </div>
+  )
+};
+
+export default CosmosView;
