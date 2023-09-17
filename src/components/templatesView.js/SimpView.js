fef5f2999fff7a92b02581770343884cd1bc2ef4
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-17 15:21:04 +0500

    feat: add simp template view

---
+++ src/components/templatesView.js/SimpView.js
@@ -0,0 +1,12 @@
+import React from "react"
+import simp from "H:\\Github\\vex-web\\src\\images\\templates\\simp.jpg"
+
+const CosmosView = (props) => {
+  return (
+    <div className="mt-10 mb-20 flex justify-center max-sm:px-8 sm:px-16 lg:px-24 2xl:px-52">
+      <img src={simp}/>
+    </div>
+  )
+};
+
+export default CosmosView;
