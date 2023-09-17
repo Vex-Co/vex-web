13d8dcde09f1afc056f76411bfefe0a9d08da826
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-17 15:19:24 +0500

    feat: added section Links and template links

--- src/components/TemplateSection.js
+++ src/components/TemplateSection.js
@@ -4,25 +4,27 @@ import simp1 from '../images/templates/simp1.jpg'
 import attari1 from '../images/templates/attari1.jpg'
 import MD1 from '../images/templates/MD1.jpg'
 import { useState } from "react"
+import { Link } from "react-router-dom"
+// import CosmosView from "./templatesView.js/CosmosView.js"
 
 const TemplateSection = (props) => {
-  const [temp1,setTemp1] = useState(false)
-  const [temp2,setTemp2] = useState(false)
-  const [temp3,setTemp3] = useState(false)
-  const [temp4,setTemp4] = useState(false)
+  const [temp1, setTemp1] = useState(false)
+  const [temp2, setTemp2] = useState(false)
+  const [temp3, setTemp3] = useState(false)
+  const [temp4, setTemp4] = useState(false)
 
-  var templatesValue = [setTemp1,setTemp2,setTemp3,setTemp4]
-  function setHoverLayer(value){
-    if(value == '1')
+  var templatesValue = [setTemp1, setTemp2, setTemp3, setTemp4]
+  function setHoverLayer(value) {
+    if (value == '1')
       setTemp1(true)
-    else if(value == '2')
+    else if (value == '2')
       setTemp2(true)
-    else if(value == '3')
+    else if (value == '3')
       setTemp3(true)
-    else if(value == '4')
+    else if (value == '4')
       setTemp4(true)
   }
-  function desetHoverLayer(){
+  function desetHoverLayer() {
     setTemp1(false)
     setTemp2(false)
     setTemp3(false)
@@ -30,7 +32,7 @@ const TemplateSection = (props) => {
   }
 
   return (
-    <div>
+    <div id="templates-section">
       <aside className='max-sm:px-8 sm:px-16 lg:px-24 2xl:px-52'>
         <div className='flex justify-center'>
           <a href="https://wa.link/xwmj6i" target="_blank">
@@ -46,74 +48,75 @@ const TemplateSection = (props) => {
         <div className='grid sm:space-y-5 md:grid-cols-2 gap-5 justify-center'>
           <div className='w-full border-black border-2 rounded-2xl mt-5 relative'>
 
-            
-          <a href="#" className="cursor-pointer">
-              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={()=>setHoverLayer("1")} onMouseLeave={desetHoverLayer}>
+
+            <Link to="/templates/cosmosView" target="_blank" className="cursor-pointer">
+              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={() => setHoverLayer("1")} onMouseLeave={desetHoverLayer}>
                 {
-                  temp1?
-                  <p>Click To Preview</p>
-                  :
-                  <div></div>
+                  temp1 ?
+                    <p>Click To Preview</p>
+                    :
+                    <div></div>
                 }
               </div>
-            </a>
+            </Link>
 
             <div>
-              <img src={cosmos1} className='w-full rounded-t-2xl' />
+              <img src={cosmos1} className='w-full rounded-t-2xl md:h-52 lg:h-60 xl:h-72 2xl:h-96' />
             </div>
             <p className="py-7 px-7 border-t-2 text-xl font-bold">Simp Template</p>
           </div>
           <div className='w-full border-black border-2 rounded-2xl mt-5 relative'>
 
-          <a href="#" className="cursor-pointer">
-              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={()=>setHoverLayer("2")} onMouseLeave={desetHoverLayer}>
+            <Link to="/templates/simpView" target="_blank" className="cursor-pointer">
+              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={() => setHoverLayer("2")} onMouseLeave={desetHoverLayer}>
                 {
-                  temp2?
-                  <p>Click To Preview</p>
-                  :
-                  <div></div>
+                  temp2 ?
+                    <p>Click To Preview</p>
+                    :
+                    <div></div>
                 }
               </div>
-            </a>
+            </Link>
             <div>
-              <img src={simp1} className='w-full rounded-t-2xl' />
+              <img src={simp1} className='w-full rounded-t-2xl md:h-52 lg:h-60 xl:h-72 2xl:h-96' />
             </div>
             <p className="py-7 px-7 border-t-2 text-xl font-bold">Simp Template</p>
           </div>
           <div className='w-full border-black border-2 rounded-2xl mt-5 relative'>
 
-            <a href="#" className="cursor-pointer">
-              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={()=>setHoverLayer("3")} onMouseLeave={desetHoverLayer}>
+            <Link to="/templates/attariView" target="_blank" className="cursor-pointer">
+              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={() => setHoverLayer("3")} onMouseLeave={desetHoverLayer}>
                 {
-                  temp3?
-                  <p>Click To Preview</p>
-                  :
-                  <div></div>
+                  temp3 ?
+                    <p>Click To Preview</p>
+                    :
+                    <div></div>
                 }
               </div>
-            </a>
+            </Link>
 
             <div>
-              <img src={attari1} className='w-full rounded-t-2xl' />
+              <img src={attari1} className='w-full rounded-t-2xl md:h-52 lg:h-60 xl:h-72 2xl:h-96' />
             </div>
             <p className="py-7 px-7 border-t-2 text-xl font-bold">Simp Template</p>
           </div>
           <div className='w-full border-black border-2 rounded-2xl mt-5 relative'>
 
-            
-          <a href="#" className="cursor-pointer">
-              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={()=>setHoverLayer("4")} onMouseLeave={desetHoverLayer}>
+
+            <Link to="/templates/MDView" target="_blank" className="cursor-pointer">
+
+              <div className="overlay rounded-2xl border-white border-2" onMouseEnter={() => setHoverLayer("4")} onMouseLeave={desetHoverLayer}>
                 {
-                  temp4?
-                  <p>Click To Preview</p>
-                  :
-                  <div></div>
+                  temp4 ?
+                    <p>Click To Preview</p>
+                    :
+                    <div></div>
                 }
               </div>
-            </a>
+            </Link>
 
             <div>
-              <img src={MD1} className='w-full rounded-t-2xl' />
+              <img src={MD1} className='w-full rounded-t-2xl md:h-52 lg:h-60 xl:h-72 2xl:h-96'/>
             </div>
             <p className="py-7 px-7 border-t-2 text-xl font-bold">Simp Template</p>
           </div>
@@ -121,7 +124,7 @@ const TemplateSection = (props) => {
         </div>
 
         <div className='flex justify-center'>
-          <button className='btnButton bg-black text-white px-10 font-medium blackHover cursor-not-allowed'>More Comming Soon</button>
+          <button className='btnButton bg-black text-white px-10 font-medium blackHover cursor-not-allowed'>More Templates Comming Soon</button>
         </div>
       </aside>
     </div>
