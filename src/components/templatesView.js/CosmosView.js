ace2b3648bdcce1aba832ea8d3ed463ef0c3af25
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-17 15:21:20 +0500

    feat: add cosmos template view

---
+++ src/components/templatesView.js/CosmosView.js
@@ -0,0 +1,12 @@
+import React from "react"
+import cosmosPc from "H:\\Github\\vex-web\\src\\images\\templates\\cosmos.png"
+
+const CosmosView = (props) => {
+  return (
+    <div className="mt-10 mb-20 max-sm:px-8 flex justify-center sm:px-16 lg:px-24 2xl:px-52">
+      <img src={cosmosPc}/>
+    </div>
+  )
+};
+
+export default CosmosView;
