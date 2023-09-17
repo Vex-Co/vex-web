0f713265943ae4342305e57140454131da54beba
Zaheer Ud Din Akram <120505597+ZaheerAkram@users.noreply.github.com>
2023-09-17 15:21:12 +0500

    feat: add MD template view

---
+++ src/components/templatesView.js/MDView.js
@@ -0,0 +1,12 @@
+import React from "react"
+import MD from "H:\\Github\\vex-web\\src\\images\\templates\\MD.jpg"
+
+const CosmosView = (props) => {
+  return (
+    <div className="mt-10 mb-20 flex justify-center max-sm:px-8 sm:px-16 lg:px-24 2xl:px-52">
+      <img src={MD}/>
+    </div>
+  )
+};
+
+export default CosmosView;
